45b926cdd416e445104c091b2fc1176a8560bd4e
bemxio <bemxiov@protonmail.com>
2023-01-02 13:28:29 +0100

    make the logo customizable (not sure if works)

--- script.js
+++ script.js
@@ -8,8 +8,54 @@ function move(image, x, y) {
     image.style.top = y;
 }
 
+// functions for getting the logo
+function getLogoURL() {
+    if (!params.has("logo")) {
+        return "/logos/default.svg"
+    }
+    
+    return params.get("logo");
+}
+
+function getLogo(url) {
+    const request = new XMLHttpRequest();
+    const parser = new DOMParser();
+    
+    request.open("GET", url, false);
+    request.send(null);
+    
+    // parse and get the SVG element
+    // if there are any errors along the way,
+    // get the default "DVD Video" logo instead
+    
+    if (request.status != 200) {
+        return getLogo("/logos/default.svg");
+    }
+    
+    let image = parser.parseFromString(request.responseText, "text/html");
+
+    if (image.querySelector("parsererror")) {
+        return getLogo("/logos/default.svg");
+    }
+    
+    image = image.firstElementChild;
+    
+    if (image == null || image.tagName != "SVG") {
+        return getLogo("/logos/default.svg");
+    }
+    
+    // filter any "fill" attributes in the SVG
+    for (const element of image.querySelectorAll("[fill]")) {
+        element.removeAttribute("fill");
+    }
+    
+    return image;
+}
+
 // constants
-const logo = document.getElementById("dvd-logo");
+const params = new URLSearchParams(window.location.search);
+
+const logo = getLogo(getLogoURL());
 const speed = 1;
 
 // variables
@@ -17,6 +63,13 @@ let x = randint(1, window.innerWidth - logo.clientWidth - 1);
 let y = randint(1, window.innerHeight - logo.clientHeight - 1);
 
 let direction = [1, 1];
+
+// set the ID and the fill color to the logo
+logo.id = "dvd-logo";
+logo.style.fill = "white";
+
+// add the logo to the page
+document.append(logo);
 
 // move the logo to the randomized initial position
 move(logo, x, y);
@@ -41,4 +94,4 @@ setInterval(() => {
     }
 
     move(logo, x, y);
-}, 0);
+});
